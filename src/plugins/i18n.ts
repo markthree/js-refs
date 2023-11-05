3a75b275b63ee6239e544b122bd9100ed65ad1a1
markthree <1801982702@qq.com>
2023-11-05 16:38:50 +0800

    fix(i18n): 持久化 i18n

--- src/plugins/i18n.ts
+++ src/plugins/i18n.ts
@@ -12,10 +12,16 @@ const messages = Object.fromEntries(
 	}),
 )
 
+// 缓存到本地 localStorage 中，第二个参数为空时的默认语言
+const storageLocale = useStorage('locale', '简体中文')
+
 export const i18n = createI18n({
 	legacy: false,
-	locale: '简体中文', // 默认语言
+	locale: storageLocale.value,
 	messages,
 })
+
+// 同步 i18n
+syncRef(storageLocale, i18n.global.locale)
 
 export default i18n
